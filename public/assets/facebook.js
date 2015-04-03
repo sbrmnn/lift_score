462d828b39c8a341abf3d41b29415d90e36e5989
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-04-02 23:11:17 -0400

    Renamed google analytics js and added facebook tracking js

---
+++ public/assets/facebook.js
@@ -0,0 +1,14 @@
+<!-- Facebook Conversion Code for Checkouts - Arun Subramanian 1 -->
+(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6020214137608', {'value':'0.01','currency':'USD'}]);
