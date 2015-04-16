ee44a583bd910fb5871befb68878e9e924d4f392
gth824c@gmail.com <arunsubramanian@Arun-Subramanians-MacBook-Pro.local>
2015-04-16 12:52:48 -0400

    Added uniqnuess validation to phone number

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -4,6 +4,7 @@ class Lead < ActiveRecord::Base
   validates_presence_of :name
   validates :email, :email => {:strict_mode => true} # Using email_validator gem to validate email field.
   validates_plausible_phone :phone_number, :presence => true
+  validates :phone_number, uniqueness: true
   after_commit :send_mail, on: :create
 
   private
